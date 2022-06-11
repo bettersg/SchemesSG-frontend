0cc94a6dc57d680d69a514a7edc82520d8bab902
R4Y815 <Akcent360@gmail.com>
2022-06-11 23:26:11 +0800

    updated for auto open accordion on first load

--- pages/listing.jsx
+++ pages/listing.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import {
   Container,
@@ -12,6 +13,7 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import FeedbackIcon from '@material-ui/icons/Feedback';
+import { uuid } from 'uuidv4';
 import { colors } from '../constants/design';
 import PageHero from '../components/Sections/PageHero';
 import Layout from '../components/Layout/Layout';
@@ -38,11 +40,42 @@ const forms = [
 ];
 
 const Listing = ({ form }) => {
-  const [expanded, setExpanded] = React.useState(form === 'feedback' ? 2 : 0);
+  // SET State Variables!
+  // to manage Accordion state-expanded or retracted
+  const [expanded, setExpanded] = useState(form === 'feedback' ? 2 : 0);
 
+  // FN to set which panel is expanded based on url:
+  const expandUrlPanel = (url) => {
+    // according to the url, get the path
+    const panelPath = url.split('/')[3];
+    // according to the url set which panel index to expand
+    switch (panelPath) {
+      case 'listing':
+        setExpanded(0);
+        break;
+      case 'listing?form=feedback#feedback':
+        setExpanded(2);
+        break;
+      default:
+        console.log('error at Listing');
+    }
+  };
+
+  // run useEffect to grab url from Window
+  // run useEffect once each time window is loaded
+  useEffect(() => {
+    // grab URL from the window.
+    // due to next, this has to run before JSX elements are created.
+    const currentURL = window.location.href;
+    console.log('currentURL =', currentURL);
+    expandUrlPanel(currentURL);
+  }, []);
+
+  // OnChange, change expanded panel to this panel if not expanded?
   const handleAccordion = (panel) => (isExpanded) => {
     setExpanded(isExpanded ? panel : 0);
   };
+
   return (
     <>
       <Layout title="Listing | Schemes SG">
@@ -56,37 +89,36 @@ const Listing = ({ form }) => {
         >
           <div className="Listing-accordions" id="listaccordions">
             {forms.map((f, i) => (
-            <Link href="/listing#listaccordions">
-              <Accordion
-                elevation={3}
-                expanded={expanded === i}
-                onChange={handleAccordion(i)}
-                key={f.name}
-                style={{ margin: '16px 0' }}
-                id={i === 2 ? 'feedback' : ''}
-                TransitionProps={{ timeout: 0, unmountOnExit: true }} 
-              >
-                <AccordionSummary
-                  expandIcon={expanded !== i && <ExpandMoreIcon />}
-                  aria-controls="panel1bh-content"  
-                  id="panel1bh-header"
+              <Link href="/listing#listaccordions" key={uuid()}>
+                <Accordion
+                  elevation={3}
+                  expanded={expanded === i}
+                  onChange={handleAccordion(i)}
+                  style={{ margin: '16px 0' }}
+                  id={i === 2 ? 'feedback' : ''}
+                  TransitionProps={{ timeout: 0, unmountOnExit: true }}
                 >
-                  <Typography
-                    variant="h6"
-                    style={{
-                      color: colors.primary.dark,
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
+                  <AccordionSummary
+                    expandIcon={expanded !== i && <ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
                   >
-                    {f.icon}
-                    {' '}
-                    {f.name}
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>{f.form}</AccordionDetails>
-              </Accordion>
-            </Link>
+                    <Typography
+                      variant="h6"
+                      style={{
+                        color: colors.primary.dark,
+                        display: 'flex',
+                        alignItems: 'center',
+                      }}
+                    >
+                      {f.icon}
+                      {' '}
+                      {f.name}
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>{f.form}</AccordionDetails>
+                </Accordion>
+              </Link>
             ))}
           </div>
         </Container>
