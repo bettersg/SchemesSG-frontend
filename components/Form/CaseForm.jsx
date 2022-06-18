9bd62b97826b23dd2b026eba400945ccab83a300
R4Y815 <Akcent360@gmail.com>
2022-06-18 21:20:51 +0800

    added Fn to clear TextFields on successful submission

--- components/Form/CaseForm.jsx
+++ components/Form/CaseForm.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // import axios from 'axios';
 import {
   Typography, Button, TextField, Snackbar,
@@ -32,15 +32,30 @@ const CaseForm = () => {
   // setState for CSS spinner, for submit button
   const [loading, setLoading] = useState(false);
 
+  // useRefs for textInput value control
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const caseRef = useRef();
+
+  // Fn for emptying Text Input fields upon successful submission
+  const clearInputs = () => {
+    nameRef.current.value = '';
+    emailRef.current.value = '';
+    caseRef.current.value = '';
+  };
+
+  // Fn for shutting off Snackbar when user selects close
   const handleClose = () => {
     // when user clicks to close SnackBar, change
     // open state to false
     setOpen(false);
+    clearInputs();
   };
 
   // create variable to call useStyles to set makeStyles
   const classes = useStyles();
 
+  // Fn for handling text input fields
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -56,6 +71,7 @@ const CaseForm = () => {
     })
       .then((response) => {
         if (response.status === 200) {
+          clearInputs();
           setOpen(true);
           setSuccess(true);
           setLoading(false);
@@ -74,7 +90,7 @@ const CaseForm = () => {
   };
   return (
     <>
-      <form className="CaseForm-root" onSubmit={handleSubmit}>
+      <form className="CaseForm-root">
         <Typography variant="body1" style={{ textAlign: 'left' }}>
           Name
         </Typography>
@@ -92,6 +108,7 @@ const CaseForm = () => {
           variant="outlined"
           required
           name="Name"
+          inputRef={nameRef}
         />
         <Typography variant="body1" style={{ textAlign: 'left' }}>
           Email (optional, if you want us to reach you)
@@ -110,6 +127,7 @@ const CaseForm = () => {
           }}
           variant="outlined"
           name="Email"
+          inputRef={emailRef}
         />
         <Typography variant="body1" style={{ textAlign: 'left' }}>
           What area requires more research?
@@ -130,19 +148,22 @@ const CaseForm = () => {
           variant="outlined"
           required
           name="Case"
+          inputRef={caseRef}
         />
         <Button
-          type="submit"
+          type="onClick"
           variant="contained"
           color="primary"
           disableElevation
+          disabled={loading} /* if loading=True, button is greyed out and not selectable */
+          onClick={handleSubmit}
         >
           <Typography variant="subtitle1" className={classes.btnText}>
-            {loading ? <CircularProgress style={{ height: 20, width: 20 }} color="#fff" /> : 'Submit'}
+            {loading ? <CircularProgress style={{ height: 20, width: 20 }} /> : 'Submit'}
           </Typography>
         </Button>
       </form>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
         <Alert onClose={handleClose} severity={success ? 'success' : 'error'}>
           {success
             ? 'We have received your message!'
