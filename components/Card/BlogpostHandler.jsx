b6836b5cb7a088179c93d9ac91660fb0b524f535
R4Y815 <Akcent360@gmail.com>
2022-06-22 20:14:58 +0800

    Making Blog Post page images web-responsive. RichText rendering switched to plain text rendering

---
+++ components/Card/BlogpostHandler.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+import Prismic from '@prismicio/client';
+import { Container } from '@material-ui/core';
+import { RichText } from 'prismic-reactjs';
+import { v4 as uuidv4 } from 'uuid';
+import { linkResolver } from '../../utils/prismic';
+import { breakpoints } from '../../constants/design';
+
+const BlogpostHandler = ({ post }) => {
+  console.log('post =', post);
+
+  return (
+    <>
+      {post.data.post_content.map((object, i) => {
+        if (object.type === 'image') { return <img src={`${object.url}`} alt={`img${i}`} key={uuidv4()} />; }
+        return (
+          <p key={uuidv4()}>
+            <section>
+              {object.text}
+            </section>
+          </p>
+        );
+      })}
+      <style jsx>
+        {`
+          img {
+            width: 90vw;
+            height: auto;
+          }
+
+
+          .Blogpost-root {
+            position: relative;
+          }
+
+          section {
+            padding: 3rem 1rem;
+            word-spacing: 3px;
+            line-height: 1.8;
+          }
+
+          @media only screen and (min-width: ${breakpoints.width.md}px) {
+            
+            section {
+              padding: 3rem 1rem;
+              word-spacing: 3px;
+              line-height: 1.8;
+              font-size: 120%
+            }
+          }
+        `}
+      </style>
+    </>
+  );
+};
+
+export default BlogpostHandler;
--- pages/blog/[uid].jsx
+++ pages/blog/[uid].jsx
@@ -1,48 +1,89 @@
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
 import React from 'react';
 import Prismic from '@prismicio/client';
 import { Container } from '@material-ui/core';
 import { RichText } from 'prismic-reactjs';
-import { client, linkResolver } from '../../utils/prismic';
+import { client } from '../../utils/prismic';
 import Layout from '../../components/Layout/Layout';
 import PageHero from '../../components/Sections/PageHero';
-
+import BlogpostHandler from '../../components/Card/BlogpostHandler';
+import DeviceDetect from '../../utils/devicedetect';
 import { breakpoints } from '../../constants/design';
 
-const Blogpost = ({ post }) => (
-  <>
-    <Layout
-      title={post.data.meta_title ? post.data.meta_title : undefined}
-      description={
+// FN to adjust clone of incoming post
+const tweakImgUrl = (array, vwPortWidth) => {
+  for (let i = 0; i < array.data.post_content.length; i += 1) {
+    if (array.data.post_content[i].type === 'image') {
+      // get width
+      const oriWidth = array.data.post_content[i].dimensions.width;
+      // get height
+      const oriHeight = array.data.post_content[i].dimensions.height;
+      // get aspect ratio (ar) of image (width: height)
+      const ar = oriWidth / oriHeight;
+      // calculate scaled height based on current
+      // viewport width
+      const newWidth = vwPortWidth;
+      const newHeight = newWidth / ar;
+
+      // replace old dim. with new dim.
+      array.data.post_content[i].dimensions.width = newWidth;
+      array.data.post_content[i].dimensions.height = newHeight;
+
+      // grab current url
+      const oriUrl = array.data.post_content[i].url;
+      const newUrl = `${oriUrl}&rect=0,0,${oriWidth.toString()},${oriHeight.toString()}&w=${newWidth.toString()}&h=${newHeight.toString()}`;
+      array.data.post_content[i].url = newUrl;
+    }
+  }
+};
+
+const Blogpost = ({ post }) => {
+  // grab viewport width before react elements are generated
+/*   useEffect(() => {
+    const currentWidth = window.innerWidth;
+    tweakImgUrl(postClone, currentWidth);
+  }, []); */
+
+  console.log('post =', post);
+  return (
+    <>
+      <Layout
+        title={post.data.meta_title ? post.data.meta_title : undefined}
+        description={
           post.data.meta_description ? post.data.meta_description : undefined
         }
-      ogTitle={
+        ogTitle={
           post.data.open_graph_title ? post.data.open_graph_title : undefined
         }
-      ogDescription={
+        ogDescription={
           post.data.open_graph_description
             ? post.data.open_graph_description
             : undefined
         }
-      ogImage={
+        ogImage={
           post.data.open_graph_image.url
             ? post.data.open_graph_image.url
             : undefined
         }
-      ogUrl={`https://schemes.sg/blog/${post.uid}`}
-    >
-      <PageHero
-        title={RichText.asText(post.data.title)}
-        subtitle={RichText.asText(post.data.excerpt)}
-      />
-      <section>
-        <Container maxWidth="lg">
-          {RichText.render(post.data.post_content, linkResolver)}
-        </Container>
-      </section>
-    </Layout>
+        ogUrl={`https://schemes.sg/blog/${post.uid}`}
+      >
+        <PageHero
+          title={RichText.asText(post.data.title)}
+          subtitle={RichText.asText(post.data.excerpt)}
+        />
+        <section>
+          <Container maxWidth="lg">
+            <BlogpostHandler post={post} />
+          </Container>
+        </section>
+      </Layout>
 
-    <style jsx>
-      {`
+      <style jsx>
+        {`
           .Blogpost-root {
             position: relative;
           }
@@ -63,9 +104,10 @@ const Blogpost = ({ post }) => (
             }
           }
         `}
-    </style>
-  </>
-);
+      </style>
+    </>
+  );
+};
 
 export default Blogpost;
 
@@ -87,10 +129,20 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const post = await client.getByUID('blog_posts', params.uid);
-
+  console.log('Device ReadOut Backend =', DeviceDetect());
+  // set all image displays to be 360px in width
+  /* tweakImgUrl(post, 360); */
   return {
     props: {
       post,
     },
+    // **** Incremental Static Regeneration (ISR)****
+    // Next.js allows you to create or update static pages after you've
+    // built your site.
+    // info: https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+    // Next.js will attempt to re-generate the page:
+    // - when a request comes in
+    // - at most once every 5 seonds
+    /* revalidate: 5, // in secs   */
   };
 }
